f1de638d070829c763f11013c575550989f57ac1
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-03-14 20:02:08 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimaScroll from './modules/anima-scroll.js';
+import initBtnMobile from './modules/btn-mobile.js';
+
+initScrollSuave();
+initAnimaScroll();
+initBtnMobile();
