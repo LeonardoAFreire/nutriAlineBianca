367fcbd6cd997e1c34968328f59db60935207a3f
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-04-06 17:05:33 -0300

    cards novo e secao quem sou

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 import initScrollSuave from './modules/scroll-suave.js';
 import initAnimaScroll from './modules/anima-scroll.js';
-import initBtnMobile from './modules/btn-mobile.js';
 import initAnimaNumeros from './modules/anima-numeros.js';
 
 initScrollSuave();
 initAnimaScroll();
-initBtnMobile();
 initAnimaNumeros();
